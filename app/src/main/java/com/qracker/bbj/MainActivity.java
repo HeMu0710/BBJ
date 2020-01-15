47d64c8291d101d9318915e026fd1237214bbc49
hemu0710 <hemu0710@qq.com>
2020-01-16 01:00:47 +0800

    small changes

--- app/src/main/java/com/qracker/bbj/MainActivity.java
+++ app/src/main/java/com/qracker/bbj/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        Log.d("nmsl","nmsl2555");
     }
 
 }
--- app/src/main/java/com/qracker/bbj/ui/accounting/AccountingFragment.java
+++ app/src/main/java/com/qracker/bbj/ui/accounting/AccountingFragment.java
@@ -1,17 +1,22 @@
 package com.qracker.bbj.ui.accounting;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -33,5 +38,18 @@ public class AccountingFragment extends Fragment {
         listView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView listView = view.findViewById(R.id.listView_accounting);
+        Adapter adapter = listView.getAdapter();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                PopupWindow popupWindow = new PopupWindow(100,100);
+            }
+        });
     }
 }
