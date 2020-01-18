8f332483eac18cb9d1fd621cab527aa392ea1ded
hemu0710 <hemu0710@qq.com>
2020-01-19 00:19:47 +0800

    test the fileoutput

--- app/src/main/java/com/qracker/bbj/MainActivity.java
+++ app/src/main/java/com/qracker/bbj/MainActivity.java
@@ -11,6 +11,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +33,30 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        BufferedWriter bw = null;
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput("data", MODE_PRIVATE);
+            bw = new BufferedWriter(new OutputStreamWriter(fos));
+            bw.write("nmsl");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
